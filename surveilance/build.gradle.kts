f43750923febf318fe5493a70d1a920a37e939b9
itsjkiprotich <keeprawteachjapheth@gmail.com>
2025-08-11 14:50:19 +0300

    UPD: add surveilance module

---
+++ surveilance/build.gradle.kts
@@ -0,0 +1,69 @@
+plugins {
+  id(Plugins.BuildPlugins.application)
+  id(Plugins.BuildPlugins.kotlinAndroid)
+  id(Plugins.BuildPlugins.navSafeArgs)
+}
+
+configureRuler()
+
+android {
+  namespace = "com.google.android.fhir.surveilance"
+  compileSdk = Sdk.COMPILE_SDK
+  defaultConfig {
+    applicationId = Releases.Demo.applicationId
+    minSdk = Sdk.MIN_SDK
+    targetSdk = Sdk.TARGET_SDK
+    versionCode = Releases.Demo.versionCode
+    versionName = Releases.Demo.versionName
+    testInstrumentationRunner = Dependencies.androidJunitRunner
+  }
+  buildTypes {
+    release {
+      isMinifyEnabled = true
+      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+    }
+  }
+  buildFeatures {
+    buildConfig = true
+    viewBinding = true
+  }
+  compileOptions {
+    // Flag to enable support for the new language APIs
+    // See https://developer.android.com/studio/write/java8-support
+    isCoreLibraryDesugaringEnabled = true
+  }
+
+  packaging { resources.excludes.addAll(listOf("META-INF/ASL-2.0.txt", "META-INF/LGPL-3.0.txt")) }
+  kotlin { jvmToolchain(11) }
+}
+
+dependencies {
+  androidTestImplementation(libs.androidx.test.espresso.core)
+  androidTestImplementation(libs.androidx.test.ext.junit)
+
+  coreLibraryDesugaring(Dependencies.desugarJdkLibs)
+
+  implementation(Dependencies.timber)
+  implementation(libs.androidx.activity)
+  implementation(libs.androidx.appcompat)
+  implementation(libs.androidx.constraintlayout)
+  implementation(libs.androidx.datastore.preferences)
+  implementation(libs.androidx.fragment)
+  implementation(libs.androidx.lifecycle.livedata)
+  implementation(libs.androidx.lifecycle.runtime.ktx)
+  implementation(libs.androidx.lifecycle.viewmodel)
+  implementation(libs.androidx.navigation.fragment)
+  implementation(libs.androidx.navigation.ui)
+  implementation(libs.androidx.recyclerview)
+  implementation(libs.androidx.work.runtime)
+  implementation(libs.kotlin.stdlib)
+  implementation(libs.kotlinx.coroutines.android)
+  implementation(libs.kotlinx.coroutines.core)
+  implementation(libs.material)
+  implementation(project(":datacapture")) {
+    exclude(group = "com.google.android.fhir", module = "engine")
+  }
+  implementation(project(":engine"))
+
+  testImplementation(libs.junit)
+}
---
+++ surveilance/src/main/java/com/google/android/fhir/surveilance/MainActivity.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2022-2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.fhir.surveilance
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.fhir.surveilance.databinding.ActivityMainBinding
+
+const val MAX_RESOURCE_COUNT = 20
+
+class MainActivity : AppCompatActivity() {
+  private lateinit var binding: ActivityMainBinding
+  private val activityViewModel: ActivityViewModel by viewModels()
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+    initActionBar()
+    activityViewModel.createPatientsOnAppFirstLaunch()
+  }
+
+  private fun initActionBar() {
+    val toolbar = binding.toolbar
+    setSupportActionBar(toolbar)
+  }
+}
