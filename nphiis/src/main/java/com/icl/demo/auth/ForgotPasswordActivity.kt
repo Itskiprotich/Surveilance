2f9562fd3079c6eb30bb3087a5e34cdb2a337e97
itskiprotich <keeprawteachjapheth@gmail.com>
2025-12-03 07:06:58 +0300

    UPD: done auth

--- nphiis/src/main/java/com/icl/demo/auth/ForgotPasswordActivity.kt
+++ nphiis/src/main/java/com/icl/demo/auth/ForgotPasswordActivity.kt
@@ -1,21 +1,114 @@
 package com.icl.demo.auth
 
+import android.app.ProgressDialog
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.icl.demo.R
+import com.icl.demo.databinding.ActivityForgotPasswordBinding
+import com.icl.demo.models.DbResetPasswordData
+import com.icl.demo.network.RetrofitCallsAuthentication
+import com.icl.demo.utils.FormatterClass
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 class ForgotPasswordActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityForgotPasswordBinding
+    private var retrofitCallsAuthentication = RetrofitCallsAuthentication()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_forgot_password)
+        binding = ActivityForgotPasswordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.apply {
+            title = "Forgot Password"
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        binding.apply {
+            btnSubmit.setOnClickListener {
+
+                val emailAddress = etEmail.text.toString()
+                if (emailAddress.isEmpty()) {
+                    emailLayout.error = "Please enter Username"
+                    etEmail.requestFocus()
+                    return@setOnClickListener
+                }
+
+
+
+                binding.emailLayout.error = null
+                val payload = DbResetPasswordData(
+                    idNumber = emailAddress,
+                    email = emailAddress
+                )
+                CoroutineScope(Dispatchers.Main).launch {
+
+                    val progressDialog = ProgressDialog(this@ForgotPasswordActivity)
+                    progressDialog.setTitle("Please wait..")
+                    progressDialog.setMessage("Validating email address..")
+                    progressDialog.setCanceledOnTouchOutside(false)
+                    progressDialog.show()
+
+                    val job = Job()
+                    CoroutineScope(Dispatchers.IO + job).launch {
+                        FormatterClass().saveSharedPref(
+                            "idNumber",
+                            emailAddress,
+                            this@ForgotPasswordActivity
+                        )
+                        val pairReturn = retrofitCallsAuthentication
+                            .getResetPassword(this@ForgotPasswordActivity, payload)
+
+                        val messageCode = pairReturn.first
+                        val messageToast = pairReturn.second
+
+                        CoroutineScope(Dispatchers.Main).launch {
+                            Toast.makeText(
+                                this@ForgotPasswordActivity, messageToast,
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            if (messageCode == 200 || messageCode == 201) {
+                                val intent = Intent(
+                                    this@ForgotPasswordActivity,
+                                    SetPasswordActivity::class.java
+                                )
+                                startActivity(intent)
+                                this@ForgotPasswordActivity.finish()
+                            }
+                        }
+
+                    }.join()
+                    progressDialog.dismiss()
+
+                }
+
+            }
+            haveCodeTextView.setOnClickListener {
+                startActivity(
+                    Intent(
+                        this@ForgotPasswordActivity,
+                        SetPasswordActivity::class.java
+                    )
+                )
+                finish()
+            }
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressedDispatcher.onBackPressed()
+        return true
     }
 }
--- nphiis/src/main/java/com/icl/demo/auth/SetPasswordActivity.kt
+++ nphiis/src/main/java/com/icl/demo/auth/SetPasswordActivity.kt
@@ -1,21 +1,120 @@
 package com.icl.demo.auth
 
+import android.app.ProgressDialog
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.icl.demo.R
+import com.icl.demo.databinding.ActivitySetPasswordBinding
+import com.icl.demo.models.DbSetPasswordReq
+import com.icl.demo.network.RetrofitCallsAuthentication
+import com.icl.demo.utils.FormatterClass
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 class SetPasswordActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySetPasswordBinding
+    private var retrofitCallsAuthentication = RetrofitCallsAuthentication()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_set_password)
+        binding = ActivitySetPasswordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.apply {
+            title = "Reset Password"
+        }
+        binding.apply {
+            btnSubmit.setOnClickListener {
+                val code = binding.codeEditText.text.toString().trim()
+                val password = binding.passwordEditText.text.toString().trim()
+                val confirm = binding.confirmPasswordEditText.text.toString().trim()
+
+                if (code.isEmpty() || password.isEmpty() || confirm.isEmpty()) {
+                    Toast.makeText(
+                        this@SetPasswordActivity,
+                        "All fields are required",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    return@setOnClickListener
+                }
+                if (code.isEmpty()) {
+                    binding.codeInputLayout.error = "Please enter the code"
+                    binding.codeEditText.requestFocus()
+                    return@setOnClickListener
+                }
+                if (password.isEmpty()) {
+                    binding.passwordInputLayout.error = "Please enter your password"
+                    binding.passwordEditText.requestFocus()
+                    return@setOnClickListener
+                }
+                if (confirm.isEmpty()) {
+                    binding.confirmPasswordInputLayout.error = "Please confirm the password"
+                    binding.confirmPasswordEditText.requestFocus()
+                    return@setOnClickListener
+                }
+                if (password != confirm) {
+                    binding.confirmPasswordInputLayout.error = "Passwords do not match"
+                    return@setOnClickListener
+                } else {
+                    binding.confirmPasswordInputLayout.error = null
+                }
+
+                CoroutineScope(Dispatchers.Main).launch {
+
+                    val progressDialog = ProgressDialog(this@SetPasswordActivity)
+                    progressDialog.setTitle("Please wait..")
+                    progressDialog.setMessage("Validation in progress..")
+                    progressDialog.setCanceledOnTouchOutside(false)
+                    progressDialog.show()
+
+                    val job = Job()
+                    CoroutineScope(Dispatchers.IO + job).launch {
+                        val idNumber =
+                            FormatterClass().getSharedPref("idNumber", this@SetPasswordActivity)
+                        val dbSetPasswordReq = DbSetPasswordReq(code, "$idNumber", password)
+                        val pairReturn = retrofitCallsAuthentication
+                            .setPassword(this@SetPasswordActivity, dbSetPasswordReq)
+
+                        val messageCode = pairReturn.first
+                        val messageToast = pairReturn.second
+
+                        CoroutineScope(Dispatchers.Main).launch {
+                            Toast.makeText(
+                                this@SetPasswordActivity, messageToast,
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            if (messageCode == 200 || messageCode == 201) {
+                                val intent = Intent(
+                                    this@SetPasswordActivity,
+                                    LoginActivity::class.java
+                                ).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                                startActivity(intent)
+                            }
+                        }
+                    }.join()
+                    progressDialog.dismiss()
+
+                }
+            }
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressedDispatcher.onBackPressed()
+        return true
     }
 }
